05fccef509bb3f7fc02b592680f0669358bc414f
Merna Hesham <87778784+MernaHesham10@users.noreply.github.com>
2025-02-22 22:26:17 +0200

    Update 1

---
+++ Code/src/test/java/calculator_application/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Create a Simple Calculator Application
+ * create a basic calculator application with four operations: addition, subtraction, multiplication, and division.
+ */
+package calculator_application;
